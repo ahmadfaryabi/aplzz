fc8cb4862619680dc8ae17c0a5f8694dbd4631c2
liban0610 <115471778+liban0610@users.noreply.github.com>
2024-10-25 10:35:57 +0200

    Koblet til databasen

---
+++ Aplzz/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Aplzz.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# aplzz
+Social media web application
+
+Stages:
+ASP NET core
+
+React (for mobile application) 
+
+test
