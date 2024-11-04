44986102feb318b2e990d9b4ce2bb607bd393a59
Ahmad Faryabi <ahmadfaryabi03@gmail.com>
2024-11-04 20:55:43 +0100

    ok

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -19,6 +19,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb6bdf35fdc0bbd3b18537fead3c5e5bc8f78f0")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2f92c6328edd3a0f9efae3ef7bd939fd4e7dee")]
@@ -51,6 +52,12 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
 >>>>>>> 86d362f (login system endring)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
+>>>>>>> a051670 (ok)
+>>>>>>> 82a25f3 (ok)
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
