eadcad2079e6e8f840f37be1595df114957186c5
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-05 14:37:33 +0100

    test

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -30,6 +30,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb6bdf35fdc0bbd3b18537fead3c5e5bc8f78f0")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2f92c6328edd3a0f9efae3ef7bd939fd4e7dee")]
@@ -83,6 +84,8 @@ using System.Reflection;
 >>>>>>> 555fc11 (commit)
 =======
 >>>>>>> a14eafd (fikset noen byggefeil)
+=======
+>>>>>>> 6cdb676 (test)
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
@@ -147,7 +150,13 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3748811b9883c77fe6841c952254a026d518e27f")]
 >>>>>>> 677e36b (fikset noen byggefeil)
+<<<<<<< HEAD
 >>>>>>> a14eafd (fikset noen byggefeil)
+=======
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677e36b4b7fd6dd53bb0119fc552ecabd06a9368")]
+>>>>>>> d9a838d (test)
+>>>>>>> 6cdb676 (test)
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
