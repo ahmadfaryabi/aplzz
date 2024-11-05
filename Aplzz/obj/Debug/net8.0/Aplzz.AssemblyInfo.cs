7b958887613eb99277c91e4ac5633679871172d2
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-05 13:23:51 +0100

    endring

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa967092d4506bb066e573074617596addd068c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
