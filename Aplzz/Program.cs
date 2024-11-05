d99f336fee5261038d837b2c088847523dcbe697
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-05 14:58:28 +0100

    endring

--- Aplzz/Program.cs
+++ Aplzz/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Aplzz.DAL;
-using Serilog;
-using Serilog.Events;
+using Aplzz.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,29 +7,19 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DbContexts>(options => {
     options.UseSqlite(
-        builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
+        builder.Configuration["ConnectionStrings:DatabaseConnection"]);
 });
 
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-
-var loggerConfiguration = new LoggerConfiguration()
-    .MinimumLevel.Information() // levels: Trace< Information < Warning < Erorr < Fatal
-    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
-
-loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
-    e.Level == LogEventLevel.Information &&
-    e.MessageTemplate.Text.Contains("Executed DbCommand"));
-
-builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddSession(options => {
+    options.Cookie.Name = ".Applz.Session";
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    DBInit.Seed(app);
-
 }
 
 app.UseStaticFiles();
