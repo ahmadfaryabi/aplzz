19808408644567f442e1ad3e40f68a6ae47090d5
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-05 14:28:48 +0100

    fikset noen byggefeil

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -29,6 +29,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb6bdf35fdc0bbd3b18537fead3c5e5bc8f78f0")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2f92c6328edd3a0f9efae3ef7bd939fd4e7dee")]
@@ -80,6 +81,8 @@ using System.Reflection;
 >>>>>>> 36bb7eb (commit)
 =======
 >>>>>>> 555fc11 (commit)
+=======
+>>>>>>> a14eafd (fikset noen byggefeil)
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
@@ -138,7 +141,13 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072e29b0b15c60a12ee95c76632f0a975e33f9e2")]
 >>>>>>> c654fa1 (commit)
+<<<<<<< HEAD
 >>>>>>> 555fc11 (commit)
+=======
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3748811b9883c77fe6841c952254a026d518e27f")]
+>>>>>>> 677e36b (fikset noen byggefeil)
+>>>>>>> a14eafd (fikset noen byggefeil)
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
