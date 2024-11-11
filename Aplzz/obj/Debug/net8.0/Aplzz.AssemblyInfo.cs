017c4f41f0a6492f7946046c912ecaeb8a76fff1
sakariam <111963152+sakariam@users.noreply.github.com>
2024-11-11 16:22:28 +0100

    accountUpdate

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa072ab6340dd49215f8b54d690763a3d98dd16")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e916e46e01e4025b2acb5965d10bdc054b692042")]
->>>>>>> c951a1d1beb8e4cef695f35a3a216086839a0545
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75277bb468f77d63e69100bdd237aa004df62952")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
