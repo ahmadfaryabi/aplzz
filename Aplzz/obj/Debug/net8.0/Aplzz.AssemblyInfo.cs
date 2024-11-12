4e879d973269bdba984f176ab72078bd983d3e99
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-12 13:59:39 +0100

    fiks på koder

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -44,6 +44,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb6bdf35fdc0bbd3b18537fead3c5e5bc8f78f0")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2f92c6328edd3a0f9efae3ef7bd939fd4e7dee")]
@@ -125,6 +126,8 @@ using System.Reflection;
 >>>>>>> 1628de3 (fikse feil)
 =======
 >>>>>>> 6f13df7 (feil fiksing)
+=======
+>>>>>>> f2790dc (fiks på koder)
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
@@ -273,7 +276,13 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6cecd9afb96acbbfb4a5413e566e3db72c62da5")]
 >>>>>>> 834f36d (feil fiksing)
+<<<<<<< HEAD
 >>>>>>> 6f13df7 (feil fiksing)
+=======
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834f36d863c846b1632f94b213a230550a152324")]
+>>>>>>> ea10153 (fiks på koder)
+>>>>>>> f2790dc (fiks på koder)
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
