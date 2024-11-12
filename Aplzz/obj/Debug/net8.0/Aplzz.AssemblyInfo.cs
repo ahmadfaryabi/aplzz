9a39966d3b1945ec0220d7baeb9f4a65710c6930
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-12 15:08:36 +0100

    ok

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -46,6 +46,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb6bdf35fdc0bbd3b18537fead3c5e5bc8f78f0")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2f92c6328edd3a0f9efae3ef7bd939fd4e7dee")]
@@ -131,6 +132,8 @@ using System.Reflection;
 >>>>>>> f2790dc (fiks på koder)
 =======
 >>>>>>> c4bfff6 (fikset)
+=======
+>>>>>>> 6ee949c (ok)
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
@@ -291,7 +294,13 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea10153a63a45f0a320ede95493be7c8b3a284de")]
 >>>>>>> 43f796e (fikset)
+<<<<<<< HEAD
 >>>>>>> c4bfff6 (fikset)
+=======
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c309488d650232045e4f5b6fd099d8bb9dbfd04")]
+>>>>>>> 1808ecd (ok)
+>>>>>>> 6ee949c (ok)
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
