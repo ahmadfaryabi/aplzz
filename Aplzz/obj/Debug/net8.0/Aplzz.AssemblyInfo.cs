46bb5baed944ca4d42e454fe0aa879f8d3b8773a
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2024-11-12 15:34:04 +0100

    fiks feil

--- Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
+++ Aplzz/obj/Debug/net8.0/Aplzz.AssemblyInfo.cs
@@ -47,6 +47,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb6bdf35fdc0bbd3b18537fead3c5e5bc8f78f0")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2f92c6328edd3a0f9efae3ef7bd939fd4e7dee")]
@@ -134,6 +135,8 @@ using System.Reflection;
 >>>>>>> c4bfff6 (fikset)
 =======
 >>>>>>> 6ee949c (ok)
+=======
+>>>>>>> 84e58dd (fiks feil)
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea908cc2caf9cd9acd1347015e3258dff095ba6a")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ab8f9e31a6f97052aac30c454f8741b71bd7bc")]
@@ -300,7 +303,13 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c309488d650232045e4f5b6fd099d8bb9dbfd04")]
 >>>>>>> 1808ecd (ok)
+<<<<<<< HEAD
 >>>>>>> 6ee949c (ok)
+=======
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7925b6c349e372554b2f565dbe28e2ba4e21883c")]
+>>>>>>> 0b2f92c (fiks feil)
+>>>>>>> 84e58dd (fiks feil)
 [assembly: System.Reflection.AssemblyProductAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
